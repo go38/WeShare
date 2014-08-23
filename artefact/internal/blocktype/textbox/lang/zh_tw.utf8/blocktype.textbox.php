5eb6273df7dba5aae49c41f27e058d60844ee5fb
Eric Hsin <eric.hsin@gmail.com>
2014-08-24 00:42:48 +0800

    Updated by Eric

---
+++ artefact/internal/blocktype/textbox/lang/zh_tw.utf8/blocktype.textbox.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2008 Catalyst IT Ltd (http://www.catalyst.net.nz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage lang/zh_tw.utf8
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @author     
+ * @copyright  
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['blockcontent'] = '區塊內容';
+$string['description'] = '增加筆記到您的頁面';
+$string['makeacopy'] = '製作副本';
+$string['managealltextboxcontent'] = '管理全部文字盒內容';
+$string['readonlymessage'] = '您選擇的文字在這頁面不能編輯。';
+$string['textusedinotherblocks'] = '如果您編輯這區塊的文字內容，也將會一起變更在其他 %s 的區塊內容。';
+$string['title'] = '文字盒';
+$string['usecontentfromanothertextbox'] = '使用其他文字盒的內容';
