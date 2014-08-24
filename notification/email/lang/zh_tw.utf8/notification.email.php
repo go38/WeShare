742bda32912a4d327e7318ed9575afcb271f698b
Eric Hsin <eric.hsin@gmail.com>
2014-08-24 08:38:12 +0800

    Update

---
+++ notification/email/lang/zh_tw.utf8/notification.email.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2008 Catalyst IT Ltd (http://www.catalyst.net.nz)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage notification-email
+ * @author     Catalyst IT Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2006-2008 Catalyst IT Ltd http://catalyst.net.nz
+ *
+ */
+
+defined('INTERNAL') || die();  
+
+$string['emailsubject'] = '%s';
+$string['emailheader'] = '你收到%s發送的通知。 以下為訊息內容:';
+$string['emailfooter'] = '這是從%s自動產生的通知。 如要更新接收通知的喜好設定, 登入 %s';
+$string['referurl'] = ' 瀏覽%s';
+$string['unsubscribemessage'] = "如要取消訂閱%s, 登入\n%s";
+
+$string['name'] = '電郵';
